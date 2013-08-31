ccbed422d38f1a31abd0015ec56e143f500851c8
Sigurd Holsen <sighol@gmail.com>
2013-08-31 14:06:11 +0200

    1: ferdig før init

--- 01/triangles.cpp
+++ 01/triangles.cpp
@@ -9,6 +9,10 @@ enum VAO_IDs {Triangles, NumVAOs};
 enum Buffer_IDs {ArrayBuffer, NumBuffers};
 enum Attrib_IDs {vPosition = 0};
 
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
+
+const GLuint NumVertices = 6;
 
 int main(int argc, char const *argv[])
 {
