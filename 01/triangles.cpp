8175d85abf91907a95c384ee5b82f9483cdadb00
Sigurd Holsen <sighol@gmail.com>
2013-08-31 14:11:06 +0200

    1: vertices

--- 01/triangles.cpp
+++ 01/triangles.cpp
@@ -5,14 +5,29 @@ using namespace std;
 #include "vgl.h"
 #include "LoadShaders.h"
 
-enum VAO_IDs {Triangles, NumVAOs};
-enum Buffer_IDs {ArrayBuffer, NumBuffers};
+enum VAO_IDs {Triangles=0, NumVAOs=1};
+enum Buffer_IDs {ArrayBuffer=0, NumBuffers=1};
 enum Attrib_IDs {vPosition = 0};
 
 GLuint VAOs[NumVAOs];
 GLuint Buffers[NumBuffers];
 
 const GLuint NumVertices = 6;
+
+void init()
+{
+	glGenVertexArrays(NumVAOs, VAOs);
+	glBindVertexArray(VAOs[Triangles]);
+
+	GLfloat vertices[NumVertices][2] = {
+		{ -0.90, -0.90}, // Triangle 1
+		{  0.85, -0.90},
+		{ -0.90,  0.85},
+		{  0.90, -0.85}, // Triangle 2
+		{  0.90,  0.90},
+		{ -0.85,  0.90}
+	};
+}
 
 int main(int argc, char const *argv[])
 {
