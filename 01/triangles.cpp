9c57a650fc7f4c760cf58d41dedc6f13c604b6ab
Sigurd Holsen <sighol@gmail.com>
2013-08-31 14:03:27 +0200

    Initial commit

---
+++ 01/triangles.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#include "vgl.h"
+#include "LoadShaders.h"
+
+enum VAO_IDs {Triangles, NumVAOs};
+enum Buffer_IDs {ArrayBuffer, NumBuffers};
+enum Attrib_IDs {vPosition = 0};
+
+
+int main(int argc, char const *argv[])
+{
+
+}
